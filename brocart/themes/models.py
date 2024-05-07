bb6682baddda561ccff53a7172a7bc29111f7010
Shiyas9961 <shiyasshiyyu9961@gmail.com>
2024-05-07 11:57:35 +0530

    added theme model

--- brocart/themes/models.py
+++ brocart/themes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Theme model
+
+class Theme (models.Model) :
+
+    banner = models.ImageField(upload_to = 'themes/')
+    caption = models.TextField()
